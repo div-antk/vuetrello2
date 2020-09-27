a791e1695824ddd6018b915ef8c5462ef4dfa8f8
div-antk <51283468+owaii@users.noreply.github.com>
2020-09-27 12:00:12 +0900

    リストの並び替えができる

--- src/components/Board.vue
+++ src/components/Board.vue
@@ -8,27 +8,30 @@
       <div class="list-index">
         <!-- listコンポーネントを呼び出し、必要なデータを渡す（propsで定義したデータ） -->
         <!-- v-vindを : として省略できる -->
-        <list v-for="(item, index) in lists"
-          :key="item.id"
-          :title="item.title"
-          :cards="item.cards"
-          :listIndex="index"
-          @change="movingCard"
-        />
-        <list-add />
+        <draggable class="list-index">
+          <list v-for="(item, index) in lists"
+            :key="item.id"
+            :title="item.title"
+            :cards="item.cards"
+            :listIndex="index"
+            @change="movingCard"
+          />
+          <list-add />
+        </draggable>
       </div>
     </main>
   </div>
 </template>
 
 <script>
-
+import draggable from 'vuedraggable'
 import ListAdd from './ListAdd.vue'
 import List from './List'
 import { mapState } from 'vuex'
 
 export default {
   components: {
+    draggable,
     ListAdd,
     List,
   },
